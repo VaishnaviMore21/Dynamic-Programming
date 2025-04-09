d0bf7224701526b7c0bc568a1148e28a8a44d235
Vaishnavi More <143414333+VaishnaviMore21@users.noreply.github.com>
2025-04-09 11:34:56 +0530

    Create 01.Climb Stairs.java

---
+++ 01.Climb Stairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+        if( n==0 || n==1) return 1;
+        int n1=climbStairs(n-1);
+        int n2=climbStairs(n-2);
+        return n1+n2;
+    }
+}
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+  
+  dp[0]= 1;
+  dp[1]= 1;
+  
+  for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+  }
+  return dp[n];
+    }
+};
